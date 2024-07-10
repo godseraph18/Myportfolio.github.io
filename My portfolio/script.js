f69c929764f5945474e7fcb5199e7f756126c90b
godseraph18 <fernandezelijahcarl@gmail.com>
2024-07-11 00:05:21 +0800

    My portfolio.

---
+++ My portfolio/script.js
@@ -0,0 +1,16 @@
+const menuIcon = document.querySelector('#menu-icon'); 
+const navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+const logoutBtn = document.querySelector(".logout-btn");
+
+logoutBtn.addEventListener("click",()=>{
+    window.location.replace('index2.html')
+})
+var username = sessionStorage.getItem('username');
+if (username) {
+    document.write('Logged in as: ' + username);
+}
